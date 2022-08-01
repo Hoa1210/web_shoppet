3592d5c75e2e42d634ebfc02d0deaab6be08cbe0
LuongHoa <luonghoa121002@gmail.com>
2022-08-01 12:54:41 +0700

    config database

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+// $config = [
+//     'database' => [
+//         'host' => 'localhost',
+//         'user' => 'root',
+//         'pass' => '',
+//         'name' => 'web_shoppet',
+//         'driver' => 'mysql',
+//     ]
+// ];
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','web_shoppet');
